062ef68bd57902f397fd05778ff1332b219149e4
Victor Ukok <ukokjnr@gmail.com>
2026-09-05 06:16:55 +0100

    Fix: Default response if there's no response from the model.

--- cli.ts
+++ cli.ts
@@ -11,40 +11,40 @@ const rl = readline.createInterface({
 
 await ingestDocument(
   "policy-001",
-  "Internal Policy: Employees at Kodehauz get 25 days of paid time off. The primary server cluster is located in Jos, Nigeria."
+  "Internal Policy: Employees at Kodehauz get 25 days of paid time off. The primary server cluster is located in Jos, Nigeria.",
 );
 
 const conversationHistory: BaseMessageListItem[] = [];
 
 console.clear();
-console.log("\x1b[36m========================================================\x1b[0m");
+console.log(
+  "\x1b[36m========================================================\x1b[0m",
+);
 console.log("\x1b[1m  🤖 Mastra AI CLI Assistant by Victor Ukok \x1b[0m");
 console.log("  Type '\x1b[33mexit\x1b[0m' or press Ctrl+C to stop.");
-console.log("\x1b[36m========================================================\x1b[0m\n");
-
-const agent = mastra.getAgent("assistantAgent");
+console.log(
+  "\x1b[36m========================================================\x1b[0m\n",
+);
 
 function promptUser() {
   rl.question("\x1b[32mYou > \x1b[0m", async (input) => {
-    const userInput = input.trim();
-
-    if (!userInput) {
-      promptUser();
-      return;
-    }
-
-    if (userInput.toLowerCase() === "exit") {
-      cleanupAndExit();
-      return;
-    }
-
-    conversationHistory.push({ role: "user", content: userInput });
-
-    process.stdout.write("\x1b[34mAI > \x1b[0m");
-
-    let fullResponse = "";
-
     try {
+      const agent = mastra.getAgent("assistantAgent");
+      const userInput = input.trim();
+      if (!userInput) {
+        promptUser();
+        return;
+      }
+
+      if (userInput.toLowerCase() === "exit") {
+        cleanupAndExit();
+        return;
+      }
+      conversationHistory.push({ role: "user", content: userInput });
+
+      process.stdout.write("\x1b[34mAI > \x1b[0m");
+
+      let fullResponse = "";
       const streamResponse = await agent.stream(conversationHistory);
 
       for await (const chunk of streamResponse.textStream) {
@@ -53,15 +53,18 @@ function promptUser() {
       }
       process.stdout.write("\n\n");
 
-      if(!fullResponse) {
-        process.stdout.write("No response at the moment, please try again.");
+      if (!fullResponse) {
+        process.stdout.write("No response at the moment, please try again with same prompt or adjust your prompt.");
         promptUser();
       }
 
       conversationHistory.push({ role: "assistant", content: fullResponse });
     } catch (error: any) {
-      console.error("\n\x1b[31m[Error generating response]:\x1b[0m", error?.message || error);
-      console.log();
+      console.error(
+        "\n\x1b[31m[Error generating response]:\x1b[0m",
+        error?.message,
+      );
+      // console.log();
     }
 
     promptUser();
